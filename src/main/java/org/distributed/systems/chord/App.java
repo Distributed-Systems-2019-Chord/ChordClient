d93b968bdfa6ea43b3d84f499bd39da4bf117448
Stijn Meijerink <stijn.meijerink@gmail.com>
2019-11-14 11:36:10 +0100

    Init maven hello world cli app

---
+++ src/main/java/org/distributed/systems/chord/App.java
@@ -0,0 +1,13 @@
+package org.distributed.systems.chord;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
