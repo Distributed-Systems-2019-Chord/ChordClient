523ed0ef93dd426628a12e6477d83e28873289a3
Stijn Meijerink <stijn.meijerink@gmail.com>
2019-11-19 14:56:34 +0100

    Added the base classes such as ChordStart and Node.

---
+++ src/main/java/org/distributed/systems/chord/node/SuccessorManagingActor.java
@@ -0,0 +1,16 @@
+package org.distributed.systems.chord.node;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+
+public class SuccessorManagingActor extends AbstractActor {
+
+    private ActorRef successor;
+
+    @Override
+    public Receive createReceive() {
+
+        return null;
+    }
+
+}
