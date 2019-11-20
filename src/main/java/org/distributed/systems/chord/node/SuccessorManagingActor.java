c3e9987b7893bb36d5ec52ec96a06540d0a4791e
Stijn Meijerink <stijn.meijerink@gmail.com>
2019-11-20 10:44:12 +0100

    Removed non compiling test, added model and messaging example classes.

--- src/main/java/org/distributed/systems/chord/node/SuccessorManagingActor.java
+++ src/main/java/org/distributed/systems/chord/node/SuccessorManagingActor.java
@@ -1,15 +1,27 @@
 package org.distributed.systems.chord.node;
 
 import akka.actor.AbstractActor;
-import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.japi.pf.FI;
+import org.distributed.systems.chord.messaging.NodeJoinMessage;
+import org.distributed.systems.chord.model.Successor;
 
 public class SuccessorManagingActor extends AbstractActor {
 
-    private ActorRef successor;
+    private Successor successor;
+
+    static Props props() {
+        return Props.create(Successor.class);
+    }
 
     @Override
     public Receive createReceive() {
+        return receiveBuilder()
+                .match(NodeJoinMessage.class, handleJoiningNode()).build();
+    }
 
+    private FI.UnitApply<NodeJoinMessage> handleJoiningNode() {
+        // TODO
         return null;
     }
 
