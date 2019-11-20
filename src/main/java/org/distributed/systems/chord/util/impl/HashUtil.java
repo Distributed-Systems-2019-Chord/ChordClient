67f520601a2034f3b839f15510d50b9f495fdc84
lucien martijn <lucien.martijn@gmail.com>
2019-11-20 12:46:08 +0100

    added hashing function

--- src/main/java/org/distributed/systems/chord/util/impl/HashUtil.java
+++ src/main/java/org/distributed/systems/chord/util/impl/HashUtil.java
@@ -2,10 +2,50 @@ package org.distributed.systems.chord.util.impl;
 
 import org.distributed.systems.chord.util.IHashUtil;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+
 public class HashUtil implements IHashUtil {
 
+    /**
+     * Hashing with SHA1
+     *
+     * @param input String to hash
+     * @return String hashed
+     */
     @Override
     public String hash(String input) {
-        return null;
+        String sha1 = "";
+        try
+        {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(input.getBytes("UTF-8"));
+            sha1 = byteToHex(crypt.digest());
+        }
+        catch(NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+    private static String byteToHex(final byte[] hash)
+    {
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
     }
 }
+
+
