526aa6b481140328c817fedf620fc367a770c936
lucien martijn <lucien.martijn@gmail.com>
2019-11-27 16:25:33 +0100

    added comments to test

--- src/test/java/org/distributed/systems/chord/test/AkkaFindSuccesorTest.java
+++ src/test/java/org/distributed/systems/chord/test/AkkaFindSuccesorTest.java
@@ -25,17 +25,22 @@ public class AkkaFindSuccesorTest {
     public void closestPrecedingFinger(){
         ChordNode centralNode_0 = new ChordNode(0L, "localhost", 8080);
         ChordNode node_3 = new ChordNode(3L, "localhost", 8082);
-        FingerTable table0 = new FingerTable(new ArrayList<>(3), 0);
-        table0.addFinger(new Finger(1L, new FingerInterval(1L,2L), node_3));
-        table0.addFinger(new Finger(2L, new FingerInterval(2L,4L), node_3));
-        table0.addFinger(new Finger(4L, new FingerInterval(4L,0L), centralNode_0));
+        FingerTable table3 = new FingerTable(new ArrayList<>(3), 0);
+        table3.addFinger(new Finger(4L, new FingerInterval(4L,5L), centralNode_0));
+        table3.addFinger(new Finger(5L, new FingerInterval(5L,7L), centralNode_0));
+        table3.addFinger(new Finger(7L, new FingerInterval(7L,3L), centralNode_0));
 
-//        FingerTable table1 = new FingerTable(new ArrayList<>(3), 0);
-//        table1.addFinger(new Finger(2L, new FingerInterval(2L,3L), node_3));
-//        table1.addFinger(new Finger(3L, new FingerInterval(3L,5L), node_3));
-//        table1.addFinger(new Finger(5L, new FingerInterval(5L,1L), centralNode_0));
+        // Suppose node 3 wants to find the successor of key 1
+        // Closest_preceding_finger will lookup in fingertable of node 3 and will find that key 1 falls within
+        // key interval 7 and 3. Expected result of this test is that centralNode_0 is the return value of
+        // closest_preceding_finger
 
-//        ChordNode node_1 = new ChordNode(1L, "localhost", 8081);
+        // TODO: Also make a test if the id cannot be found in the finger table.
+
+        // expected = centralNode_0.closest_preceding_finger(1);
+        long expectedNodeId = 0L;
+
+        // assertEquals(expectedNodeId, expected.getId());
 
 
 
